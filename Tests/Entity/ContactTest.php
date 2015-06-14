10d960c3fb727c4c09ced4a718e7bc217e26b077
epfremme <epfremme@nerdery.com>
2015-06-13 21:48:18 -0500

    Added Contact entity test & test bootstrap

---
+++ Tests/Entity/ContactTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * File ContactTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Tests\Entity;
+
+use Epfremmer\SwaggerBundle\Entity\Contact;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+
+/**
+ * Class ContactTest
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Tests\Entity
+ */
+class ContactTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Contact
+     */
+    protected $contact;
+
+    /**
+     * @var Serializer
+     */
+    protected static $serializer;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->contact = new Contact();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$serializer = SerializerBuilder::create()->build();
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Contact::setName
+     */
+    public function testSetName()
+    {
+        $this->assertClassHasAttribute('name', Contact::class);
+        $this->assertInstanceOf(Contact::class, $this->contact->setName('foo'));
+        $this->assertAttributeEquals('foo', 'name', $this->contact);
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Contact::getName
+     * @depends testSetName
+     */
+    public function testGetName()
+    {
+        $this->contact->setName('foo');
+        $this->assertEquals('foo', $this->contact->getName());
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Contact::setUrl
+     */
+    public function testSetUrl()
+    {
+        $this->assertClassHasAttribute('url', Contact::class);
+        $this->assertInstanceOf(Contact::class, $this->contact->setUrl('foo'));
+        $this->assertAttributeEquals('foo', 'url', $this->contact);
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Contact::getUrl
+     * @depends testSetUrl
+     */
+    public function testGetUrl()
+    {
+        $this->contact->setUrl('foo');
+        $this->assertEquals('foo', $this->contact->getUrl());
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Contact::setEmail
+     */
+    public function testSetEmail()
+    {
+        $this->assertClassHasAttribute('email', Contact::class);
+        $this->assertInstanceOf(Contact::class, $this->contact->setEmail('foo'));
+        $this->assertAttributeEquals('foo', 'email', $this->contact);
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Contact::getEmail
+     * @depends testSetEmail
+     */
+    public function testGetEmail()
+    {
+        $this->contact->setEmail('foo');
+        $this->assertEquals('foo', $this->contact->getEmail());
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Contact
+     */
+    public function testDeserialize()
+    {
+        $data = json_encode((object)[
+            'name'  => 'foo',
+            'url'   => 'bar',
+            'email' => 'baz',
+        ]);
+
+        $contact = self::$serializer->deserialize($data, Contact::class, 'json');
+
+        $this->assertInstanceOf(Contact::class, $contact);
+        $this->assertAttributeEquals('foo', 'name', $contact);
+        $this->assertAttributeEquals('bar', 'url', $contact);
+        $this->assertAttributeEquals('baz', 'email', $contact);
+    }
+}
