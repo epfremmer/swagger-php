03b0f1dbae50a27b4ada8852d51cf1ce76966777
epfremme <epfremme@nerdery.com>
2015-06-14 17:29:06 -0500

    Added empty Boolean and Null primitive trait tests

---
+++ Tests/Entity/Schemas/Primitives/BooleanPrimitiveTraitTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * File BooleanPrimitiveTraitTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Tests\Entity\Schemas\Primitives;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Epfremmer\SwaggerBundle\Entity\Schemas\Primitives\BooleanPrimitive;
+
+/**
+ * Class BooleanPrimitiveTraitTest
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Tests\Entity\Schemas\Primitives
+ */
+class BooleanPrimitiveTraitTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var BooleanPrimitive|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $mockTrait;
+
+    /**
+     * Mock Classname
+     * @var string
+     */
+    protected $mockClass;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->mockTrait = $this->getMockForTrait(BooleanPrimitive::class);
+        $this->mockClass = get_class($this->mockTrait);
+    }
+}
---
+++ Tests/Entity/Schemas/Primitives/NullPrimitiveTraitTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * File NullPrimitiveTraitTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Tests\Entity\Schemas\Primitives;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Epfremmer\SwaggerBundle\Entity\Schemas\Primitives\NullPrimitive;
+
+/**
+ * Class NullPrimitiveTraitTest
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Tests\Entity\Schemas\Primitives
+ */
+class NullPrimitiveTraitTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var NullPrimitive|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $mockTrait;
+
+    /**
+     * Mock Classname
+     * @var string
+     */
+    protected $mockClass;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->mockTrait = $this->getMockForTrait(NullPrimitive::class);
+        $this->mockClass = get_class($this->mockTrait);
+    }
+}
