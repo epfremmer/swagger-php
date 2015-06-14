4b9f4f0660c3aedc6f1b0b32353d4753e51752ca
epfremme <epfremme@nerdery.com>
2015-06-13 20:17:49 -0500

    Added initial License entity

---
+++ Epfremmer/SwaggerBundle/Entity/License.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * File License.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Entity;
+
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class License
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Entity
+ */
+class License
+{
+
+    /**
+     * @JMS\Type("string")
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @JMS\Type("string")
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return Contact
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+}
