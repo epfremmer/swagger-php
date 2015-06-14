58e62755ce7c6430fec23d4ca8a79c698751942c
epfremme <epfremme@nerdery.com>
2015-06-13 22:25:30 -0500

    Added initial License entity test

---
+++ Tests/Entity/LicenseTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * File LicenseTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Tests\Entity;
+
+use Epfremmer\SwaggerBundle\Entity\License;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+
+/**
+ * Class LicenseTest
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Tests\Entity
+ */
+class LicenseTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var License
+     */
+    protected $license;
+
+    /**
+     * @var Serializer
+     */
+    protected static $serializer;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->license = new License();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$serializer = SerializerBuilder::create()->build();
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\License::getName
+     * @covers Epfremmer\SwaggerBundle\Entity\License::setName
+     */
+    public function testName()
+    {
+        $this->assertClassHasAttribute('name', License::class);
+        $this->assertInstanceOf(License::class, $this->license->setName('foo'));
+        $this->assertAttributeEquals('foo', 'name', $this->license);
+        $this->assertEquals('foo', $this->license->getName());
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\License::getUrl
+     * @covers Epfremmer\SwaggerBundle\Entity\License::setUrl
+     */
+    public function testUrl()
+    {
+        $this->assertClassHasAttribute('url', License::class);
+        $this->assertInstanceOf(License::class, $this->license->setUrl('foo'));
+        $this->assertAttributeEquals('foo', 'url', $this->license);
+        $this->assertEquals('foo', $this->license->getUrl());
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Contact
+     */
+    public function testDeserialize()
+    {
+        $data = json_encode((object)[
+            'name' => 'foo',
+            'url'  => 'bar',
+        ]);
+
+        $license = self::$serializer->deserialize($data, License::class, 'json');
+
+        $this->assertInstanceOf(License::class, $license);
+        $this->assertAttributeEquals('foo', 'name', $license);
+        $this->assertAttributeEquals('bar', 'url', $license);
+    }
+}
