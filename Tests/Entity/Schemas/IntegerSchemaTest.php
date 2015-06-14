62a0d323075ddd1b69c7de340ce1303336e981d6
epfremme <epfremme@nerdery.com>
2015-06-14 16:45:41 -0500

    Added JMS serialization tests to schema test classes

--- Tests/Entity/Schemas/IntegerSchemaTest.php
+++ Tests/Entity/Schemas/IntegerSchemaTest.php
@@ -6,8 +6,11 @@
  */
 namespace Epfremmer\SwaggerBundle\Tests\Entity\Schemas;
 
+use Epfremmer\SwaggerBundle\Entity\ExternalDocumentation;
 use Epfremmer\SwaggerBundle\Entity\Schemas\AbstractSchema;
 use Epfremmer\SwaggerBundle\Entity\Schemas\IntegerSchema;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
 
 /**
  * Class IntegerSchemaTest
@@ -24,11 +27,24 @@ class IntegerSchemaTest extends \PHPUnit_Framework_TestCase
     protected $integerSchema;
 
     /**
+     * @var Serializer
+     */
+    protected static $serializer;
+
+    /**
      * {@inheritdoc}
      */
     protected function setUp()
     {
         $this->integerSchema = new IntegerSchema();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$serializer = SerializerBuilder::create()->build();
     }
 
     /**
@@ -38,5 +54,34 @@ class IntegerSchemaTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertNotEmpty($this->integerSchema->getType());
         $this->assertEquals(AbstractSchema::INTEGER_TYPE, $this->integerSchema->getType());
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Schemas\BooleanSchema
+     */
+    public function testSerialization()
+    {
+        $data = json_encode([
+            'type' => AbstractSchema::INTEGER_TYPE,
+            'format'      => 'foo',
+            'title'       => 'bar',
+            'description' => 'baz',
+            'example'     => 'qux',
+            'externalDocs' => (object)[],
+        ]);
+
+        $schema = self::$serializer->deserialize($data, AbstractSchema::class, 'json');
+
+        $this->assertInstanceOf(IntegerSchema::class, $schema);
+        $this->assertAttributeEquals('foo', 'format', $schema);
+        $this->assertAttributeEquals('bar', 'title', $schema);
+        $this->assertAttributeEquals('baz', 'description', $schema);
+        $this->assertAttributeEquals('qux', 'example', $schema);
+        $this->assertAttributeInstanceOf(ExternalDocumentation::class, 'externalDocs', $schema);
+
+        $json = self::$serializer->serialize($schema, 'json');
+
+        $this->assertJson($json);
+        $this->assertJsonStringEqualsJsonString($data, $json);
     }
 }
