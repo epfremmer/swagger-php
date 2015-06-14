cb563086b41c25228c0baa7b579556c2bc981bf9
epfremme <epfremme@nerdery.com>
2015-06-13 22:27:52 -0500

    Added initial Tag and ExternalDocumentation entities

---
+++ Entity/ExternalDocumentation.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * File Tag.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Entity;
+
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class Tag
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Entity
+ */
+class ExternalDocumentation
+{
+
+    /**
+     * @JMS\Type("string")
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @JMS\Type("string")
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return ExternalDocumentation
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return ExternalDocumentation
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+}
---
+++ Entity/Tag.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * File Tag.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Entity;
+
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class Tag
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Entity
+ */
+class Tag
+{
+
+    /**
+     * @JMS\Type("string")
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @JMS\Type("string")
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @JMS\Type("Epfremmer\SwaggerBundle\Entity\ExternalDocumentation")
+     * @var ExternalDocumentation
+     */
+    protected $externalDocs;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Tag
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Tag
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return ExternalDocumentation
+     */
+    public function getExternalDocs()
+    {
+        return $this->externalDocs;
+    }
+
+    /**
+     * @param ExternalDocumentation $externalDocs
+     * @return Tag
+     */
+    public function setExternalDocs(ExternalDocumentation $externalDocs)
+    {
+        $this->externalDocs = $externalDocs;
+        return $this;
+    }
+}
