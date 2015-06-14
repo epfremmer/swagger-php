e1717da58fc4cce6879abeb66380bc6e1fb0c3bb
epfremme <epfremme@nerdery.com>
2015-06-14 16:29:44 -0500

    Added initial RefSchema entity and test

---
+++ Entity/Schemas/RefSchema.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * File RefSchema.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Entity\Schemas;
+
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class RefSchema
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Entity\Schemas
+ */
+class RefSchema extends AbstractSchema
+{
+    use Primitives\AnyPrimitive;
+
+    /**
+     * @JMS\Type("string")
+     * @JMS\SerializedName("$ref")
+     * @var string
+     */
+    protected $ref;
+
+    /**
+     * @return string
+     */
+    public function getRef()
+    {
+        return $this->ref;
+    }
+
+    /**
+     * @param string $ref
+     * @return AbstractSchema
+     */
+    public function setRef($ref)
+    {
+        $this->ref = $ref;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return AbstractSchema::REF_TYPE;
+    }
+}
