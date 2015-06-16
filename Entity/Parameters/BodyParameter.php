c07fe0e658f3ec95be65c04c918bac073c320ba7
epfremme <epfremme@nerdery.com>
2015-06-16 00:30:38 -0500

    Restored body parameter entity

---
+++ Entity/Parameters/BodyParameter.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * File BodyParameter.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Entity\Parameters;
+
+use Epfremmer\SwaggerBundle\Entity\Schemas\AbstractSchema;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class BodyParameter
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Entity\Parameters
+ */
+class BodyParameter extends AbstractParameter
+{
+
+    /**
+     * @JMS\Type("Epfremmer\SwaggerBundle\Entity\Schemas\AbstractSchema")
+     * @var AbstractSchema
+     */
+    protected $schema;
+
+    /**
+     * @return AbstractSchema
+     */
+    public function getSchema()
+    {
+        return $this->schema;
+    }
+
+    /**
+     * @param AbstractSchema $schema
+     * @return AbstractParameter
+     */
+    public function setSchema(AbstractSchema $schema)
+    {
+        $this->schema = $schema;
+        return $this;
+    }
+}
