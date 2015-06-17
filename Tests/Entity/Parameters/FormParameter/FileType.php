4afea0f36e8a688eda10af1c66e03395da79cb19
epfremme <epfremme@nerdery.com>
2015-06-16 23:33:34 -0500

    Added FileType form data parameter test

---
+++ Tests/Entity/Parameters/FormParameter/FileType.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * File FileTypeTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Tests\Entity\Parameters\FormParameter;
+
+use Epfremmer\SwaggerBundle\Entity\Parameters\AbstractParameter;
+use Epfremmer\SwaggerBundle\Entity\Parameters\AbstractTypedParameter;
+use Epfremmer\SwaggerBundle\Entity\Parameters\FormParameter;
+use Epfremmer\SwaggerBundle\Tests\Mixin\SerializerContextTrait;
+
+/**
+ * Class FileTypeTest
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Tests\Entity\Parameters\FormParameter
+ */
+class FileTypeTest extends \PHPUnit_Framework_TestCase
+{
+    use SerializerContextTrait;
+
+    /**
+     * @var FormParameter\FileType
+     */
+    protected $parameter;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->parameter = new FormParameter\FileType();
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Parameters\FormParameter\FileType
+     */
+    public function testSerialization()
+    {
+        $data = json_encode([
+            'in'   => AbstractParameter::IN_FORM_DATA,
+            'type' => AbstractTypedParameter::FILE_TYPE,
+            'name'             => 'foo',
+            'description'      => 'bar',
+            'required'         => false,
+            'format'           => 'baz',
+            'allowEmptyValues' => true,
+            'default'          => false,
+        ]);
+
+        $parameter = self::$serializer->deserialize($data, AbstractParameter::class, 'json');
+
+        $this->assertInstanceOf(FormParameter\FileType::class, $parameter);
+        $this->assertAttributeEquals(AbstractParameter::IN_FORM_DATA, 'in', $parameter);
+        $this->assertAttributeEquals(AbstractTypedParameter::FILE_TYPE, 'type', $parameter);
+        $this->assertAttributeEquals('foo', 'name', $parameter);
+        $this->assertAttributeEquals('bar', 'description', $parameter);
+        $this->assertAttributeEquals(false, 'required', $parameter);
+        $this->assertAttributeEquals('baz', 'format', $parameter);
+        $this->assertAttributeEquals(true, 'allowEmptyValues', $parameter);
+        $this->assertAttributeEquals(false, 'default', $parameter);
+
+        $json = self::$serializer->serialize($parameter, 'json');
+
+        $this->assertJson($json);
+        $this->assertJsonStringEqualsJsonString($data, $json);
+    }
+}
