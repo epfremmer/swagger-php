70b2767ce3e81a3180c5f7a5c295e15fa688b40f
epfremme <epfremme@nerdery.com>
2015-06-16 23:54:22 -0500

    Added all typed header parameter tests

---
+++ Tests/Entity/Parameters/HeaderParameter/ArrayTypeTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * File ArrayTypeTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Tests\Entity\Parameters\HeaderParameter;
+
+use Epfremmer\SwaggerBundle\Entity\Parameters\AbstractParameter;
+use Epfremmer\SwaggerBundle\Entity\Parameters\AbstractTypedParameter;
+use Epfremmer\SwaggerBundle\Entity\Parameters\HeaderParameter;
+use Epfremmer\SwaggerBundle\Tests\Mixin\SerializerContextTrait;
+
+/**
+ * Class ArrayTypeTest
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Tests\Entity\Parameters\HeaderParameter
+ */
+class ArrayTypeTest extends \PHPUnit_Framework_TestCase
+{
+    use SerializerContextTrait;
+
+    /**
+     * @var HeaderParameter\ArrayType
+     */
+    protected $parameter;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->parameter = new HeaderParameter\ArrayType();
+    }
+
+    /**
+     * @covers Epfremmer\SwaggerBundle\Entity\Parameters\HeaderParameter\ArrayType
+     */
+    public function testSerialization()
+    {
+        $data = json_encode([
+            'in'   => AbstractParameter::IN_HEADER,
+            'type' => AbstractTypedParameter::ARRAY_TYPE,
+            'name'             => 'foo',
+            'description'      => 'bar',
+            'required'         => false,
+            'format'           => 'baz',
+            'allowEmptyValues' => true,
+            'default'          => false,
+        ]);
+
+        $parameter = self::$serializer->deserialize($data, AbstractParameter::class, 'json');
+
+        $this->assertInstanceOf(HeaderParameter\ArrayType::class, $parameter);
+        $this->assertAttributeEquals(AbstractParameter::IN_HEADER, 'in', $parameter);
+        $this->assertAttributeEquals(AbstractTypedParameter::ARRAY_TYPE, 'type', $parameter);
+        $this->assertAttributeEquals('foo', 'name', $parameter);
+        $this->assertAttributeEquals('bar', 'description', $parameter);
+        $this->assertAttributeEquals(false, 'required', $parameter);
+        $this->assertAttributeEquals('baz', 'format', $parameter);
+        $this->assertAttributeEquals(true, 'allowEmptyValues', $parameter);
+        $this->assertAttributeEquals(false, 'default', $parameter);
+
+        $json = self::$serializer->serialize($parameter, 'json');
+
+        $this->assertJson($json);
+        $this->assertJsonStringEqualsJsonString($data, $json);
+    }
+}
