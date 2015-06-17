6cc32b27a9e5c1ec0af3f0c1082b2208c3d149f8
epfremme <epfremme@nerdery.com>
2015-06-17 17:17:09 -0500

    Added swagger factory

---
+++ Factory/SwaggerFactory.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * File SwaggerFactory.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremmer\SwaggerBundle\Factory;
+
+use Epfremmer\SwaggerBundle\Entity\Swagger;
+use Epfremmer\SwaggerBundle\Parser\SwaggerParser;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
+use JMS\Serializer\EventDispatcher\EventDispatcher;
+use Epfremmer\SwaggerBundle\Listener\SerializationSubscriber;
+
+/**
+ * Class SwaggerFactory
+ *
+ * @package Epfremmer\SwaggerBundle
+ * @subpackage Factory
+ */
+class SwaggerFactory
+{
+
+    /**
+     * @var Serializer
+     */
+    protected $serializer;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $serializerBuilder = new SerializerBuilder();
+
+        $serializerBuilder->configureListeners(function(EventDispatcher $eventDispatcher) {
+            $eventDispatcher->addSubscriber(new SerializationSubscriber());
+        });
+
+        $this->serializer = $serializerBuilder->build();
+    }
+
+    /**
+     * Build Swagger document from parser interface
+     *
+     * @param string $file
+     * @return Swagger
+     */
+    public function build($file)
+    {
+        $swagger = new SwaggerParser($file);
+        $context = new DeserializationContext();
+
+        $context->setVersion(
+            $swagger->getVersion()
+        );
+
+        return $this->serializer->deserialize($swagger, Swagger::class, 'json', $context);
+    }
+
+    /**
+     * Return serialized Swagger document
+     *
+     * @param Swagger $swagger
+     * @return string
+     */
+    public function serialize(Swagger $swagger)
+    {
+        $context = new SerializationContext();
+
+        $context->setVersion(
+            $swagger->getVersion()
+        );
+
+        return $this->serializer->serialize($swagger, 'json', $context);
+    }
+}
