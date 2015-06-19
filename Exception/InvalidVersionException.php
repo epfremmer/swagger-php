4b415e7e2c2a7e7a0ffeb8f0631f426abe6464c5
epfremme <epfremme@nerdery.com>
2015-06-19 01:51:11 -0500

    Added invalid version exception class

---
+++ Exception/InvalidVersionException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * File InvalidVersionException.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace ERP\Swagger\Exception;
+
+/**
+ * Class InvalidVersionException
+ *
+ * @package ERP\Swagger
+ * @subpackage Exception
+ */
+class InvalidVersionException extends \OutOfBoundsException
+{
+
+    // custom exception message
+    const EXCEPTION_MESSAGE = "Swagger version '%s' is not supported. Please upgrade to version 2.0 or higher";
+
+    /**
+     *
+     * @param null $version
+     */
+    public function __construct($version = null)
+    {
+        parent::__construct(sprintf(self::EXCEPTION_MESSAGE, $version));
+    }
+}
